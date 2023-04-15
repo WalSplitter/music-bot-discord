0e1edb15b872f51955d61f8b766b8432efa3123c
WalSplitter <michael1913@live.de>
2023-04-15 14:03:17 +0200

    fixed deprecated codes

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 
 const {REST} = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { Client, Intents, Collection } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { Player } = require("discord-player");
 
 const fs = require('fs');
 const path = require('path');
 
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES]
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates]
 });
 
 // List of all commands
